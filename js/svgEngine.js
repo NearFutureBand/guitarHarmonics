70a447d233f10f951dbba1b7e88f43b3f2b6d88e
Paul White <theflashofsonic@gmail.com>
2018-09-24 02:46:47 +0300

    harmonic finder function

--- js/svgEngine.js
+++ js/svgEngine.js
@@ -3,12 +3,21 @@ class Neck {
         this.minWidth = 800;
         this.maxFret = maxFret;
         this.stringsCount = stringsCount;
+        /*Полная октава*/
         this.noteSequence = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+        /*Последовательность нот для настройки гитары*/
         this.defaultTuning = ["Z","E","B","G","D","A","E","B","G"];
-        this.zeroFretNotes = this.defaultTuning.slice(0, this.stringsCount + 1);
+        /*Матрица грифа для хранения данных для отрисовки*/
         this.frets = [];
+        /*Правило минорной гаммы*/
+        this.minorHarmRule = "ТПТТПТТ";
+        /*Правило мажорной гаммы*/
+        this.majorHarmRule = "ТТПТТТП";
+        
+        /*Открытые ноты при выбранном количестве струн*/
+        this.zeroFretNotes = this.defaultTuning.slice(0, this.stringsCount + 1);
+        /*Заполение массива frets*/
         this.setFretsData();
-        console.log(this.frets);
     }
     
     drawScheme() {
@@ -25,12 +34,12 @@ class Neck {
             stringGroup = d3.select('#string-' + j);
             
             stringGroup.append('g').attr('class','frets');
-            stringGroup.append('g').attr('class','lights'); 
+            if(j != 0) stringGroup.append('g').attr('class','lights'); 
             stringGroup.append('g').attr('class','notes');
             
             this.setFrets(stringGroup, j);
             this.setNotes(stringGroup, j);
-            this.setLights(stringGroup, j);
+            if(j != 0) this.setLights(stringGroup, j);
         }
         this.restyle();
     }
@@ -41,7 +50,7 @@ class Neck {
         screenWidth = ( neckWidth > this.minWidth ) ? neckWidth : this.minWidth,
         fretWidth = screenWidth / ( this.maxFret + 1 ),
         fretHeight = fretWidth * 0.5,
-        fontSize = fretHeight / 2.5,
+        fontSize = fretHeight / 3,
         lightRadius = fretWidth / 5,
         textPaddingBottom = fretHeight * 0.05,
         textAlignCoeff = fretWidth * 0.05, 
@@ -73,6 +82,36 @@ class Neck {
         }
     }
     
+    /*Находит все ноты, входящие в заданную гамму*/
+    findHarmonic(note, type) {
+        let selection = [];
+        if( note != '-') {
+            let currentNote = note,
+            rule = (type == 'Minor') ? this.minorHarmRule : this.majorHarmRule;
+        
+            for( let i = 0; i < rule.length; i++) {
+                currentNote = this.getNextNote(currentNote, rule[i]);  
+                selection.push(currentNote);
+            }
+        }
+        this.lightUpNotes(selection);   
+    }
+    
+    /*Подсвечивает все ноты, которые находятся в массиве selection*/
+    lightUpNotes(selection) {
+        this.shutDownLights();
+        
+        selection.forEach( function(note) {
+            d3.selectAll('circle[data-note = ' + note + ']')
+                .attr('fill','rgba(255,255,0,.9)')
+        });
+    }
+    
+    menuUnity() {
+        
+    }
+    
+    
     /*private*/
     setFrets(mountPlace, stringNumber) {
         mountPlace.select('g.frets')
@@ -80,8 +119,8 @@ class Neck {
             .data(this.frets[stringNumber])
             .enter().append('rect')
                 .attr('class', 'fret')
-                .attr('fill', (d, i) => (stringNumber != 0 && i != 0)? 'transparent' : 'rgba(0,0,0,.2)')
-                .attr('stroke-width', 1)
+                .attr('fill', (d, i) => (stringNumber != 0 && i != 0)? 'transparent' : 'rgba(0,0,0,.04)')
+                .attr('stroke-width', 0.3)
                 .attr('stroke', 'rgba(0,0,0,.9)')
             .exit().remove();
     }
@@ -93,19 +132,18 @@ class Neck {
                 .text( (d) => d)
                 .attr('class', 'note')
                 .attr('font-family','arial')
+                .attr('fill','rgba(0,0,0,.7)')
             .exit().remove();
     }
     setLights(mountPlace, stringNumber) {
-        if(stringNumber != 0) {
-            mountPlace.select('g.lights')
-                .selectAll('circ.light')
-                .data(this.frets[stringNumber])
-                .enter().append('circle')
-                    .attr('class', 'light')
-                    .attr('fill', 'rgb(255,255,0,.9)')
-                    .attr('data-note', (d) => d)
-                .exit().remove();
-        }
+        mountPlace.select('g.lights')
+            .selectAll('circ.light')
+            .data(this.frets[stringNumber])
+            .enter().append('circle')
+                .attr('class', 'light')
+                .attr('fill', 'transparent')
+                .attr('data-note', (d) => d)
+            .exit().remove();
     }
     setFretsData() {
         let i = 0, j = 0,
@@ -135,11 +173,14 @@ class Neck {
         }
         return this.noteSequence[ this.checkIndex(index)];
     }
+    shutDownLights() {
+        d3.selectAll('circle.light')
+            .attr('fill', 'transparent')
+    }
 }
 
 /*TODO--------------
     инициализация меню со стартовыми параметрами
-    разная ширина блоков меню
     константы из рестайла вынести в переменные-параметры
 --------------------*/
 
@@ -148,7 +189,7 @@ class Neck {
 var neck;
 
 window.addEventListener('load', function() {
-    neck = new Neck(12, 6);
+    neck = new Neck(19, 6);
     neck.drawScheme();
 });
 window.addEventListener('resize', function() {
