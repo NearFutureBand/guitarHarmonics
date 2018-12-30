552100264d6f005b7325ea26dfd8999b1553658f
Pavel Belyakov <theflashofsonic@gmail.com>
2018-12-30 18:55:59 +0300

    Template for React version

---
+++ React/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import '../css/App.less';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="page">
+          <h1>React starter template</h1>
+        </div>
+        
+      </div>
+    );
+  }
+}
+export default App;
---
+++ React/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
