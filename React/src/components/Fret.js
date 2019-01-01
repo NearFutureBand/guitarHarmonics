997bc663931380bfef4d8648c66de97e2fb6d179
Pavel Belyakov <theflashofsonic@gmail.com>
2019-01-01 18:46:57 +0300

    basic incrustation

--- React/src/components/Fret.js
+++ React/src/components/Fret.js
@@ -11,6 +11,19 @@ class Fret extends Component {
     this.state = {
       active: false
     }
+
+    this.incrustate = {
+      '3':true,
+      '5':true,
+      '7':true,
+      '9':true,
+      '12':true,
+      '15':true,
+      '17':true,
+      '19':true,
+      '21':true,
+      '24':true
+    };
   }
 
   toggleLight = () => {
@@ -21,10 +34,14 @@ class Fret extends Component {
     const { pos, tuning, harmonic } = this.props;
     const note = getNote(pos, tuning);
     const isNumber = pos[0] === 0;
-
+    const incrustated = JSON.stringify(pos[1]) in this.incrustate
 
     return (
-      <div className="fret" id={`fret-${pos[0]}-${pos[1]}`} onClick={this.toggleLight}>
+      <div
+        className={`fret ${ incrustated? 'incrustated' : '' }`}
+        id={`fret-${pos[0]}-${pos[1]}`}
+        onClick={this.toggleLight}
+      >
         <span className="note">
           {
             !isNumber? note : pos[1]
