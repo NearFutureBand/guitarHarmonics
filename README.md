90b64ed3bb3df03cd85fa7666c41fa9de09245dc
Pavel Belyakov <theflashofsonic@gmail.com>
2019-06-24 17:42:51 +0300

    removing old React version

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ This app helps young guitarists to learn harmonics!
 This repository has some versions of same application. The most functionality now is in the React version.
 * The first version created with using only vanilla js and framework [nw.js](https://nwjs.io/) for creating cross-platform applications, the graphic part is made with html blocks and flexbox approach in styling
 * The second one has the graphic part powered by [d3.js](https://d3js.org/) - awesome library to work with svg, the logics is again on vanilla js
-* (in progress) The third version will be based on the Vue.js framework
-* Version on the React framework
+* The third version is based on the Vue.js framework with state management via Vuex
+* Version on the React framework with Redux
 
 ## Developers
 
--- React/src/index.js
+++ React/src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-import reducers from './reducers';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import './index.scss';
+import App from './app/App';
+import * as serviceWorker from './serviceWorker';
+import reducer from './reducer';
 
-import App from './components/App';
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
