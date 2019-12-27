651a8c70e671aefb9ec6b5d0e785e471863b7781
Pavel Belyakov <theflashofsonic@gmail.com>
2019-12-27 18:53:02 +0300

    React: specified port for developing with local backend

--- React/src/api/index.js
+++ React/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const API_ADDRESS = 'https://guitar-scales-api.theflashofsonic.now.sh/api/';
+//export const API_ADDRESS = 'http://localhost:3000/api/';
 const axiosInstance = axios.create({
   baseURL: API_ADDRESS,
   timeout: 3000,
